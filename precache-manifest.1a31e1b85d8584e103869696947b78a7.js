4ef4eba6ac1c7aa441f2ea13d7b3b533925f8a2f
michelle12369 <p5i7gaaaa@gmail.com>
2019-10-04 17:09:07 +0800

    Updates

---
+++ precache-manifest.1a31e1b85d8584e103869696947b78a7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4566b6007aadfec4e48c4da4986fc8d",
+    "url": "/Michelle12369/booking/index.html"
+  },
+  {
+    "revision": "bdade860cf4a3bfe255a",
+    "url": "/Michelle12369/booking/static/css/2.a482237f.chunk.css"
+  },
+  {
+    "revision": "f2b1991e6c1c76264e6e",
+    "url": "/Michelle12369/booking/static/css/main.1a61d449.chunk.css"
+  },
+  {
+    "revision": "bdade860cf4a3bfe255a",
+    "url": "/Michelle12369/booking/static/js/2.09d2bea4.chunk.js"
+  },
+  {
+    "revision": "f2b1991e6c1c76264e6e",
+    "url": "/Michelle12369/booking/static/js/main.3ee005d3.chunk.js"
+  },
+  {
+    "revision": "28c303bba8a57d40ea22",
+    "url": "/Michelle12369/booking/static/js/runtime-main.763e3f4b.js"
+  }
+]);
